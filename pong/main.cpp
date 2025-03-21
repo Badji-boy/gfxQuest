2741b52480ba52cb3a2f539c5770d659e7e8ee89
Даня <dania.beloskov@mail.ru>
2025-03-21 17:28:31 +0700

    items and collisions

--- pong/main.cpp
+++ pong/main.cpp
@@ -9,18 +9,19 @@ using namespace std;
 
 
 // секция данных игры  
+
+enum class equip {
+    sword,
+    axe,
+    helmet
+};
+
 typedef struct {
     float x, y, width, height, rad, dx, dy, speed;
     HBITMAP hBitmap;//хэндл к спрайту шарика 
 } sprite;
 
 sprite racket;//ракетка игрока
-sprite ball;//шарик
-
-struct {
-    int score, balls;//количество набранных очков и оставшихся "жизней"
-    bool action = false;//состояние - ожидание (игрок должен нажать пробел) или игра
-} game;
 
 struct {
     HWND hWnd;//хэндл окна
@@ -31,118 +32,12 @@ struct {
 HBITMAP hBack;// хэндл для фонового изображения
 
 //cекция кода
-int currentLocation = 0;
 
-struct Location_
-{
-
-    HBITMAP hBack;
-    int LeftPort;
-    int RightPort;
-
+struct Item {
+    equip name;
+    sprite sprite;
+    bool isPicked;
 };
-
-Location_ location[5];
-
-
-void InitGame()
-{
-    //в этой секции загружаем спрайты с помощью функций gdi
-    //пути относительные - файлы должны лежать рядом с .exe 
-    //результат работы LoadImageA сохраняет в хэндлах битмапов, рисование спрайтов будет произовдиться с помощью этих хэндлов
-    ball.hBitmap = (HBITMAP)LoadImageA(NULL, "ball.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-    racket.hBitmap = (HBITMAP)LoadImageA(NULL, "racket.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-    //------------------------------------------------------
-    location[0].hBack = (HBITMAP)LoadImageA(NULL, "loc0.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-    location[0].RightPort = 1;
-    location[0].LeftPort = -1;
-
-
-    location[1].hBack = (HBITMAP)LoadImageA(NULL, "loc1.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-    location[1].RightPort = 2;
-    location[1].LeftPort = 0;
-
-
-    location[2].hBack = (HBITMAP)LoadImageA(NULL, "loc2.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-    location[2].RightPort = 3;
-    location[2].LeftPort = 1;
-
-    location[3].hBack = (HBITMAP)LoadImageA(NULL, "loc3.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-    location[3].RightPort = 4;
-    location[3].LeftPort = 2;
-
-    location[4].hBack = (HBITMAP)LoadImageA(NULL, "loc4.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-    location[3].RightPort = 5;
-    location[3].LeftPort = 3;
-
-
-    racket.width = 50;
-    racket.height = 50;
-    racket.speed = 30;//скорость перемещения ракетки
-    racket.x = window.width / 2.;//ракетка посередине окна
-    racket.y = window.height - racket.height;//чуть выше низа экрана - на высоту ракетки
-
-}
-
-void ProcessSound(const char* name)//проигрывание аудиофайла в формате .wav, файл должен лежать в той же папке где и программа
-{
-    //PlaySound(TEXT(name), NULL, SND_FILENAME | SND_ASYNC);//переменная name содежрит имя файла. флаг ASYNC позволяет проигрывать звук паралельно с исполнением программы
-}
-
-void ShowScore()
-{
-    //поиграем шрифтами и цветами
-    SetTextColor(window.context, RGB(160, 160, 160));
-    SetBkColor(window.context, RGB(0, 0, 0));
-    SetBkMode(window.context, TRANSPARENT);
-    auto hFont = CreateFont(70, 0, 0, 0, FW_BOLD, 0, 0, 0, 0, 0, 0, 2, 0, "CALIBRI");
-    auto hTmp = (HFONT)SelectObject(window.context, hFont);
-
-    char txt[32];//буфер для текста
-    _itoa_s(game.score, txt, 10);//преобразование числовой переменной в текст. текст окажется в переменной txt
-    TextOutA(window.context, 10, 10, "Score", 5);
-    TextOutA(window.context, 200, 10, (LPCSTR)txt, strlen(txt));
-
-    _itoa_s(game.balls, txt, 10);
-    TextOutA(window.context, 10, 100, "Balls", 5);
-    TextOutA(window.context, 200, 100, (LPCSTR)txt, strlen(txt));
-}
-
-void ProcessInput()
-{
-    if (GetAsyncKeyState(VK_LEFT)) racket.x -= racket.speed;
-    if (GetAsyncKeyState(VK_RIGHT)) racket.x += racket.speed;
-    if (GetAsyncKeyState(VK_UP)) racket.y -= racket.speed;
-    if (GetAsyncKeyState(VK_DOWN)) racket.y += racket.speed;
-
-
-    if (racket.x < 0)
-    {
-        if (location[currentLocation].LeftPort >= 0)
-        {
-            racket.x = window.width - racket.width;
-            currentLocation = location[currentLocation].LeftPort;
-        }
-        else
-        {
-            racket.x = 0;
-        }
-    }
-
-    if (racket.x > window.width)
-    {
-        if (location[currentLocation].RightPort >= 0)
-        {
-            racket.x = 0;
-            currentLocation = location[currentLocation].RightPort;
-        }
-        else
-        {
-            racket.x = window.width;
-        }
-    }
-
-}
 
 void ShowBitmap(HDC hDC, int x, int y, int x1, int y1, HBITMAP hBitmapBall, bool alpha = false)
 {
@@ -172,12 +67,182 @@ void ShowBitmap(HDC hDC, int x, int y, int x1, int y1, HBITMAP hBitmapBall, bool
     DeleteDC(hMemDC); // Удаляем контекст памяти
 }
 
+int currentLocation = 0;
+
+struct Location_ {
+    HBITMAP hBack; 
+    int LeftPort; 
+    int RightPort;
+    vector<Item> locationItems;
+};
+
+Location_ location[5];
+
+struct player_
+{
+
+    vector<equip> hero_items;
+    int count_horse;
+    int life;
+
+    void Init()
+    {
+        count_horse = 1;
+        life = 1;
+    }
+
+};
+
+player_ player;
+
+void PickItem(Item& item) {
+    if (!item.isPicked) {
+        player.hero_items.push_back(item.name); 
+        item.isPicked = true; 
+        
+    }
+}
+
+void CheckCollisions() {
+    for (auto& i : location[currentLocation].locationItems) {
+        if (!i.isPicked) {
+            
+            if (racket.x < i.sprite.x + i.sprite.width &&
+                racket.x + racket.width > i.sprite.x &&
+                racket.y < i.sprite.y + i.sprite.height &&
+                racket.y + racket.height > i.sprite.y) {
+                PickItem(i); 
+            }
+        }
+    }
+}
+
+
+
+void InitGame()
+{
+    //в этой секции загружаем спрайты с помощью функций gdi
+    //пути относительные - файлы должны лежать рядом с .exe 
+    //результат работы LoadImageA сохраняет в хэндлах битмапов, рисование спрайтов будет произовдиться с помощью этих хэндлов
+   
+    racket.hBitmap = (HBITMAP)LoadImageA(NULL, "racket.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    //------------------------------------------------------
+    location[0].hBack = (HBITMAP)LoadImageA(NULL, "loc0.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    location[0].RightPort = 1;
+    location[0].LeftPort = -1;
+    Item helmetItem;
+    helmetItem.name = equip::helmet;
+    helmetItem.sprite.hBitmap = (HBITMAP)LoadImageA(NULL, "helmet.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    helmetItem.sprite.x = 100;
+    helmetItem.sprite.y = 100;
+    helmetItem.sprite.width = 50;
+    helmetItem.sprite.height = 50;
+    helmetItem.isPicked = false;
+    location[0].locationItems.push_back(helmetItem);
+
+
+    location[1].hBack = (HBITMAP)LoadImageA(NULL, "loc1.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    location[1].RightPort = 2;
+    location[1].LeftPort = 0;
+
+
+    location[2].hBack = (HBITMAP)LoadImageA(NULL, "loc2.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    location[2].RightPort = 3;
+    location[2].LeftPort = 1;
+
+    location[3].hBack = (HBITMAP)LoadImageA(NULL, "loc3.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    location[3].RightPort = 4;
+    location[3].LeftPort = 2;
+
+    location[4].hBack = (HBITMAP)LoadImageA(NULL, "loc4.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    location[3].RightPort = -1;
+    location[3].LeftPort = 3;
+
+
+    racket.width = 50;
+    racket.height = 50;
+    racket.speed = 30;//скорость перемещения ракетки
+    racket.x = window.width / 2.;//ракетка посередине окна
+    racket.y = window.height - racket.height;//чуть выше низа экрана - на высоту ракетки
+
+    
+    
+
+}
+
+
+
+void ProcessSound(const char* name)//проигрывание аудиофайла в формате .wav, файл должен лежать в той же папке где и программа
+{
+    //PlaySound(TEXT(name), NULL, SND_FILENAME | SND_ASYNC);//переменная name содежрит имя файла. флаг ASYNC позволяет проигрывать звук паралельно с исполнением программы
+}
+
+
+
+void ProcessInput()
+{
+    if (GetAsyncKeyState(VK_LEFT)) racket.x -= racket.speed;
+    if (GetAsyncKeyState(VK_RIGHT)) racket.x += racket.speed;
+    if (GetAsyncKeyState(VK_UP)) racket.y -= racket.speed;
+    if (GetAsyncKeyState(VK_DOWN)) racket.y += racket.speed;
+
+    if (racket.y < 0)
+    {
+        racket.y = 0;
+    }
+    if (racket.y > window.height - racket.height)
+    {
+        racket.y = window.height - racket.height;
+    }
+
+    if (racket.x < 0)
+    {
+        if (location[currentLocation].LeftPort >= 0)
+        {
+            racket.x = window.width - racket.width;
+            currentLocation = location[currentLocation].LeftPort;
+        }
+        else
+        {
+            racket.x = 0;
+        }
+    }
+
+    if (racket.x > window.width - racket.width)
+    {
+        if (location[currentLocation].RightPort >= 0)
+        {
+            racket.x = 0;
+            currentLocation = location[currentLocation].RightPort;
+        }
+        else
+        {
+            racket.x = window.width - racket.width;
+        }
+    }
+
+}
+
+
 void ShowRacketAndBall()
 {
     ShowBitmap(window.context, 0, 0, window.width, window.height, location[currentLocation].hBack);//задний фон
     ShowBitmap(window.context, racket.x - racket.width / 2., racket.y, racket.width, racket.height, racket.hBitmap);// ракетка игрока
+    
 
+}
 
+void ShowItem()
+{
+    for (auto i : location[currentLocation].locationItems)
+    {
+        if (!i.isPicked)
+        {
+            ShowBitmap(window.context, i.sprite.x, i.sprite.y, i.sprite.width, i.sprite.height, i.sprite.hBitmap, true);
+        }
+        
+    }
+   
 }
 
 
@@ -212,10 +277,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     while (!GetAsyncKeyState(VK_ESCAPE))
     {
         ShowRacketAndBall();//рисуем фон, ракетку и шарик
-        ShowScore();//рисуем очик и жизни
+        ShowItem();
         BitBlt(window.device_context, 0, 0, window.width, window.height, window.context, 0, 0, SRCCOPY);//копируем буфер в окно
         Sleep(16);//ждем 16 милисекунд (1/количество кадров в секунду)
-
+        CheckCollisions();
         ProcessInput();//опрос клавиатуры
 
     }
