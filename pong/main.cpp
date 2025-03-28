5dbadf23252854a7e430891caf414db4817a61b9
Даня <dania.beloskov@mail.ru>
2025-03-28 16:54:21 +0700

    drow health and door

--- pong/main.cpp
+++ pong/main.cpp
@@ -10,18 +10,27 @@ POINT mouse;
 
 // секция данных игры  
 
-enum class equip {
+enum class equip
+{
     sword,
     axe,
-    helmet
+    helmet,
+    key
 };
+
+//enum class objects
+//{
+//    door1
+//};
 
 typedef struct {
     float x, y, width, height, rad, dx, dy, speed;
-    HBITMAP hBitmap;//хэндл к спрайту шарика 
+    HBITMAP hBitmap;
 } sprite;
 
 sprite racket;//ракетка игрока
+sprite door1;
+sprite healing;
 
 struct {
     HWND hWnd;//хэндл окна
@@ -35,8 +44,14 @@ HBITMAP hBack;// хэндл для фонового изображения
 
 struct Item {
     equip name;
-    sprite sprite;
+    sprite itemSprite;
 };
+
+//struct Object
+//{
+//    objects title;
+//    sprite objectsSprite;
+//};
 
 string getIDString(equip itemID)
 {
@@ -49,6 +64,9 @@ string getIDString(equip itemID)
         break;
     case equip::helmet:
         return "helmet";
+        break;
+    case equip::key:
+        return "key";
         break;
     default:
         return "Unknown item\n";
@@ -92,6 +110,7 @@ struct Location_ {
     int LeftPort; 
     int RightPort;
     vector<Item> locationItems;
+    vector<sprite> locationObjects;
 };
 
 Location_ location[5];
@@ -100,18 +119,16 @@ struct player_
 {
 
     vector<Item> hero_items;
-    int count_horse;
-    int life;
 
-    void Init()
-    {
-        count_horse = 1;
-        life = 1;
-    }
-
+    HBITMAP hHealthFull, hHealthEmpty;
+    int health_width = 40;
+    int max_lives = 5;  
+    int current_lives = 3;  
 };
 
 player_ player;
+
+
 
 void PickItem(int id) {
 
@@ -122,8 +139,8 @@ void PickItem(int id) {
 
 void DropItem(int ID)
 {
-    player.hero_items[ID].sprite.x = racket.x+100;
-    player.hero_items[ID].sprite.y = racket.y;
+    player.hero_items[ID].itemSprite.x = racket.x+100;
+    player.hero_items[ID].itemSprite.y = racket.y;
     location[currentLocation].locationItems.push_back(player.hero_items[ID]);
         player.hero_items.erase(player.hero_items.begin() + ID);
 
@@ -171,29 +188,59 @@ void PrintInventory()
             SetTextColor(window.context, RGB(0, 0, 250));
         }
 
-        TextOutA(window.context, x, y, s.c_str(), s.size());
+        TextOutA(window.context, x, y + 20, s.c_str(), s.size());
 
     }
     
 }
 
+bool CheckCollision(float x1, float y1, float w1, float h1,
+    float x2, float y2, float w2, float h2)
+{
+    return x1 < x2 + w2 &&
+        x1 + w1 > x2 &&
+        y1 < y2 + h2 &&
+        y1 + h1 > y2;
+}
 
 
-
-void CheckCollisions() {
+void ItemCheckCollisions() 
+{
     int id = 0;
     for (auto& i : location[currentLocation].locationItems) 
     {
-        if (racket.x < i.sprite.x + i.sprite.width &&
-            racket.x + racket.width > i.sprite.x &&
-            racket.y < i.sprite.y + i.sprite.height &&
-            racket.y + racket.height > i.sprite.y) 
+        if (CheckCollision (racket.x, racket.y, racket.width, racket.height, i.itemSprite.x, i.itemSprite.y, i.itemSprite.width, i.itemSprite.height))
         {
             PickItem(id); 
         }
 
         id++;
     }
+}
+
+void ObjectCheckCollisions()
+{
+    int id = 0;
+    for (auto& i : location[currentLocation].locationObjects)
+    {
+        if (CheckCollision(racket.x, racket.y, racket.width, racket.height, i.x, i.y, i.width, i.height))
+        {
+            if (i.hBitmap == door1.hBitmap)
+            {
+                currentLocation = location[currentLocation].RightPort;
+                racket.x = 0;
+            }
+            if (i.hBitmap == healing.hBitmap)
+            {
+                player.current_lives++;
+                location[currentLocation].locationObjects.erase(location[currentLocation].locationObjects.begin() + id);
+            }
+
+        }
+        id++;
+
+    }
+
 }
 
 
@@ -205,33 +252,58 @@ void InitGame()
     //результат работы LoadImageA сохраняет в хэндлах битмапов, рисование спрайтов будет произовдиться с помощью этих хэндлов
    
     racket.hBitmap = (HBITMAP)LoadImageA(NULL, "racket.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    door1.hBitmap = (HBITMAP)LoadImageA(NULL, "racket.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    healing.hBitmap = (HBITMAP)LoadImageA(NULL, "racket.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    player.hHealthFull = (HBITMAP)LoadImageA(NULL, "racket.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    player.hHealthEmpty = (HBITMAP)LoadImageA(NULL, "ball.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
     //------------------------------------------------------
     location[0].hBack = (HBITMAP)LoadImageA(NULL, "loc0.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
     location[0].RightPort = 1;
     location[0].LeftPort = -1;
     Item helmetItem;
     helmetItem.name = equip::helmet;
-    helmetItem.sprite.hBitmap = (HBITMAP)LoadImageA(NULL, "helmet.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-    helmetItem.sprite.x = 100;
-    helmetItem.sprite.y = 100;
-    helmetItem.sprite.width = 50;
-    helmetItem.sprite.height = 50;
+    helmetItem.itemSprite.hBitmap = (HBITMAP)LoadImageA(NULL, "helmet.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    helmetItem.itemSprite.x = 100;
+    helmetItem.itemSprite.y = 100;
+    helmetItem.itemSprite.width = 50;
+    helmetItem.itemSprite.height = 50;
     
     location[0].locationItems.push_back(helmetItem);
 
     Item axeItem;
     axeItem.name = equip::axe;
-    axeItem.sprite.hBitmap = (HBITMAP)LoadImageA(NULL, "helmet.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-    axeItem.sprite.x = 300;
-    axeItem.sprite.y = 100;
-    axeItem.sprite.width = 50;
-    axeItem.sprite.height = 50;
+    axeItem.itemSprite.hBitmap = (HBITMAP)LoadImageA(NULL, "axe.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    axeItem.itemSprite.x = 300;
+    axeItem.itemSprite.y = 100;
+    axeItem.itemSprite.width = 50;
+    axeItem.itemSprite.height = 50;
     location[0].locationItems.push_back(axeItem);
+
+    healing.width = 40;
+    healing.height = 40;
+    healing.x = 700;
+    healing.y = 400;
+    location[0].locationObjects.push_back(healing);
 
 
     location[1].hBack = (HBITMAP)LoadImageA(NULL, "loc1.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
     location[1].RightPort = 2;
     location[1].LeftPort = 0;
+
+    Item keyItem;
+    keyItem.name = equip::key;
+    keyItem.itemSprite.hBitmap = (HBITMAP)LoadImageA(NULL, "racket_enemy.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    keyItem.itemSprite.x = 600;
+    keyItem.itemSprite.y = 200;
+    keyItem.itemSprite.width = 50;
+    keyItem.itemSprite.height = 50;
+    location[1].locationItems.push_back(keyItem);
+    door1.width = 100;
+    door1.height = 500;
+    door1.x = window.width - door1.width;
+    door1.y = (window.height - door1.height) / 2.;
+    
+    location[1].locationObjects.push_back(door1);
 
 
     location[2].hBack = (HBITMAP)LoadImageA(NULL, "loc2.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
@@ -243,8 +315,8 @@ void InitGame()
     location[3].LeftPort = 2;
 
     location[4].hBack = (HBITMAP)LoadImageA(NULL, "loc4.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-    location[3].RightPort = -1;
-    location[3].LeftPort = 3;
+    location[4].RightPort = -1;
+    location[4].LeftPort = 3;
 
 
     racket.width = 50;
@@ -252,9 +324,6 @@ void InitGame()
     racket.speed = 30;//скорость перемещения ракетки
     racket.x = window.width / 2.;//ракетка посередине окна
     racket.y = window.height - racket.height;//чуть выше низа экрана - на высоту ракетки
-
-    
-    
 
 }
 
@@ -282,35 +351,56 @@ void ProcessInput()
     {
         racket.y = window.height - racket.height;
     }
+    
 
-    if (racket.x < 0)
-    {
-        if (location[currentLocation].LeftPort >= 0)
+        if (racket.x < 0)
         {
-            racket.x = window.width - racket.width;
-            currentLocation = location[currentLocation].LeftPort;
+            if (location[currentLocation].LeftPort >= 0)
+            {
+                racket.x = window.width - racket.width;
+                currentLocation = location[currentLocation].LeftPort;
+            }
+            else
+            {
+                racket.x = 0;
+            }
         }
-        else
+
+        if (racket.x > window.width - racket.width)
         {
-            racket.x = 0;
+            if (location[currentLocation].RightPort >= 0)
+            {
+                racket.x = 0;
+                currentLocation = location[currentLocation].RightPort;
+            }
+            else
+            {
+                racket.x = window.width - racket.width;
+            }
         }
     }
 
-    if (racket.x > window.width - racket.width)
-    {
-        if (location[currentLocation].RightPort >= 0)
-        {
-            racket.x = 0;
-            currentLocation = location[currentLocation].RightPort;
-        }
-        else
-        {
-            racket.x = window.width - racket.width;
-        }
-    }
 
+void DrawHealth() {
+    int margin = 10;
+    int startX = window.width - 50;
+    int startY = 10;
+
+    for (int i = 0; i < player.max_lives; i++) {
+        
+        HBITMAP currentHealth = (i < player.current_lives) ? player.hHealthFull : player.hHealthEmpty;
+
+        
+        ShowBitmap(
+            window.context,
+            startX - (i * (player.health_width + margin)), 
+            startY,
+            player.health_width,
+            player.health_width,
+            currentHealth
+        );
+    }
 }
-
 
 void ShowRacketAndBall()
 {
@@ -322,15 +412,22 @@ void ShowRacketAndBall()
 
 void ShowItem()
 {
-    for (auto i : location[currentLocation].locationItems)
+    for (const auto& i : location[currentLocation].locationItems)
     {
-        //if (!i.isPicked)
-        {
-            ShowBitmap(window.context, i.sprite.x, i.sprite.y, i.sprite.width, i.sprite.height, i.sprite.hBitmap);
-        }
-        
+       ShowBitmap(window.context, i.itemSprite.x, i.itemSprite.y, i.itemSprite.width, i.itemSprite.height, i.itemSprite.hBitmap);
     }
    
+}
+
+void ShowObjects()
+{
+    for (const auto& i : location[currentLocation].locationObjects)
+    {
+        
+            ShowBitmap(window.context, i.x, i.y, i.width, i.height, i.hBitmap);
+            
+        
+    }
 }
 
 
@@ -366,7 +463,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     {
         ShowRacketAndBall();//рисуем фон, ракетку и шарик
         ShowItem();
+        ShowObjects();
         PrintInventory();
+        DrawHealth();
 
         
         GetCursorPos(&mouse);
@@ -377,7 +476,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
         BitBlt(window.device_context, 0, 0, window.width, window.height, window.context, 0, 0, SRCCOPY);//копируем буфер в окно
         Sleep(16);//ждем 16 милисекунд (1/количество кадров в секунду)
-        CheckCollisions();
+        ItemCheckCollisions();
+        ObjectCheckCollisions();
         ProcessInput();//опрос клавиатуры
 
     }
